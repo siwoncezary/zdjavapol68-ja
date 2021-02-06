be93b5c2189618bef275c44989a079b209cc0fb2
cs <siwon.cezary@gmail.com>
2021-02-06 14:00:44 +0100

    end of day 1

--- src/main/java/interfaces/AmphibianDemo.java
+++ src/main/java/interfaces/AmphibianDemo.java
@@ -2,6 +2,10 @@ package interfaces;
 
 public class AmphibianDemo {
     public static void main(String[] args) {
-
+        Amphibian swimmingTank = new Pt76();
+        Movable tank = new Pt76();
+        swimmingTank.travel(100);
+        Swimmable boat = new Pt76();
+        boat.start();
     }
 }
